9f91b691a82adbf96717e3954a057821c4ddd78e
kalupas226 <kalupas226@gmail.com>
2023-04-08 14:20:24 +0900

    Add project

---
+++ HasDependenciesProject/HasDependenciesProject/HasDependenciesProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  HasDependenciesProjectApp.swift
+//  HasDependenciesProject
+//
+//  Created by kalupas226 on 2023/04/08.
+//
+
+import SwiftUI
+
+@main
+struct HasDependenciesProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
